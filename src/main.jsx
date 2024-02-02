71137f5e3415949630b9d50070b999e2ec4f52e5
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2024-02-02 14:39:42 -0600

    router object built

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+// import React & React Router
+import { ReactDOM } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import pages
+import App from './App';
+import Error from './pages/Error';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact'
+
+// Build Route object
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <About />,
+            }.
+            {
+                path: '/Portfolio',
+                element: <Portfolio />,
+            },
+            {
+                path: '/Contact',
+                element: <Contact />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
